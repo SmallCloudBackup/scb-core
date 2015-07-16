abf2426fbd955bed79d0eaa6989d768c41e1ee22
Valentin Alexeev <valentin.alekseev@gmail.com>
2015-07-16 17:25:39 +0300

    First common API.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * SmallCloudBackup - Core data and model
  */
 
-var core = {
-    model: require("./lib/model"),
-};
+var core = {};
+
+core.model = new require("./lib/model")(core);
+core.core = new require("./lib/core")(core);
 
 module.exports = core;
