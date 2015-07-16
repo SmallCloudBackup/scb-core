d6db71261bcfc4b307d58f1239e3c0013e37b050
Valentin Alexeev <valentin.alekseev@gmail.com>
2015-07-16 23:22:53 +0300

    Major: new interface component to enable module communication.

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ var core = {};
 
 core.model = new require("./lib/model")(core);
 core.core = new require("./lib/core")(core);
+core.iface = new require("./lib/interface")(core);
 
 module.exports = core;
